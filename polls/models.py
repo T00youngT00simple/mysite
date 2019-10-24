bc7a7e0858ca4b86bfa01285be8256460bf0f190
haha <940512354@qq.com>
2019-10-24 15:39:03 +0800

    model change

--- polls/models.py
+++ polls/models.py
@@ -25,3 +25,23 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
+
+class Student(models.Model):
+    FRESHMAN = 'FR'
+    SOPHOMORE = 'SO'
+    JUNIOR = 'JR'
+    SENIOR = 'SR'
+    YEAR_IN_SCHOOL_CHOICES = [
+        (FRESHMAN, 'Freshman'),
+        (SOPHOMORE, 'Sophomore'),
+        (JUNIOR, 'Junior'),
+        (SENIOR, 'Senior'),
+    ]
+    year_in_school = models.CharField(
+        max_length=2,
+        choices=YEAR_IN_SCHOOL_CHOICES,
+        default=FRESHMAN,
+    )
+
+    def is_upperclass(self):
+        return self.year_in_school in (self.JUNIOR, self.SENIOR)
